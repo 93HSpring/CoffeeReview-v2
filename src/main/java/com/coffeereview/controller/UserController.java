9a706fe353cbb6fa5d65e0dff11e7ce1e91eecf7
legendmic2 <goonoo0731@gmail.com>
2020-11-21 17:31:09 +0900

    add: User 관련 클래스들 생성
    -UserController, UserVO, UserMapper 생성
    -앞으로의 기능 테스트를 위한 user_index.jsp 생성
    
    #13

--- src/main/java/com/coffeereview/controller/UserController.java
+++ src/main/java/com/coffeereview/controller/UserController.java
@@ -1,6 +1,17 @@
 package com.coffeereview.controller;
 
+import java.io.IOException;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.coffeereview.domain.UserVO;
+import com.coffeereview.service.UserService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
 /**
 * @packageName	: com.coffeereview.controller
@@ -13,6 +24,68 @@ package com.coffeereview.controller;
 * -----------------------------------------------------------
 * 2020.11.21        Goonoo Jang       최초 생성
 */
+
+@Controller
+@Log4j
+@RequestMapping("/user/*")
+@AllArgsConstructor
 public class UserController {
 
+	private UserService service;
+	
+	@GetMapping("/user_index")
+	public void mainPage(Model model) {
+		log.info("/user_index");
+	}
+	
+	@GetMapping("/insertUser")
+	public String insertUser(UserVO vo) throws IOException {
+		log.info("/user");
+		
+		return "home";
+		
+	}
+	
+	@GetMapping("/readUser")
+	// READ : Id에 해당하는 user 받아오기
+	public String readUser(String uid) {
+		
+		
+		return "home";
+	}
+	
+
+	
+	@GetMapping("/updateUser")
+	// UPDATE : user 정보를 갱신하기
+	public String updateUser(UserVO vo) {
+		
+		
+		return "home";
+	}
+	
+	
+	@GetMapping("/deleteUser")
+
+	// DELETE : Id에 해당하는 user 삭제하기
+	public String deleteUser(String uid) {
+		
+		
+		return "home";
+	}
+	
+	
+	@GetMapping("/findUser")
+
+	// Id에 해당하는 user가 존재하는지 확인하기
+	public String findUser(String uid) {
+		
+		
+		return "home";
+	}
+	
+	
+	
+	
+	
 }
---
+++ src/main/java/com/coffeereview/service/UserServiceImpl.java
@@ -0,0 +1,68 @@
+package com.coffeereview.service;
+
+import org.springframework.stereotype.Service;
+
+import com.coffeereview.domain.UserVO;
+import com.coffeereview.mapper.UserMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+/**
+* @packageName	: com.coffeereview.service
+* @fileName		: UserServiceImpl.java
+* @author		: Goonoo Jang
+* @date			: 2020.11.21
+* @description	:
+* ===========================================================
+* DATE              AUTHOR             NOTE
+* -----------------------------------------------------------
+* 2020.11.21        Goonoo Jang       최초 생성
+*/
+
+@Log4j
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService{
+	
+	// 스프링 4.3 이상에서는 단일 파라미터를 받는 생성자의 경우에는
+	// 필요한 파라미터를 자동으로 주입할 수 있다.
+	// @AllArgsConstructor가 모든 파라미터를 이용하는 생성자를 만들어 준다.
+	private UserMapper mapper;
+
+	// CREATE : user를 새로 등록하기
+	@Override
+	public void insertUser(UserVO vo) {
+		
+	}
+	
+	// READ : Id에 해당하는 user 받아오기
+	public UserVO readUser(String uid) {
+		
+		log.info("readUser.........");
+		
+		return mapper.readUser(uid);
+	}
+	
+	// UPDATE : user 정보를 갱신하기
+	public void updateUser(UserVO vo) {
+		log.info("updateUser.........");
+		
+		mapper.updateUser(vo);
+	}
+	
+	// DELETE : Id에 해당하는 user 삭제하기
+	public int deleteUser(String uid) {
+		log.info("deleteUser.........");
+		
+		return mapper.deleteUser(uid);
+	}
+	
+	// Id에 해당하는 user가 존재하는지 확인하기
+	public boolean findUser(String uid) {
+		log.info("findUser.........");
+		
+		return mapper.findUser(uid);
+	}
+	
+}
