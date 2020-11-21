568a4299fd13df12f37968e41d2ac339f482c058
legendmic2 <goonoo0731@gmail.com>
2020-11-21 15:30:33 +0900

    add: feature branch 추가

---
+++ src/main/java/com/coffeereview/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.coffeereview.controller;
+
+
+
+/**
+* @packageName	: com.coffeereview.controller
+* @fileName		: UserController.java
+* @author		: Goonoo Jang
+* @date			: 2020.11.21
+* @description	:
+* ===========================================================
+* DATE              AUTHOR             NOTE
+* -----------------------------------------------------------
+* 2020.11.21        Goonoo Jang       최초 생성
+*/
+public class UserController {
+
+}
