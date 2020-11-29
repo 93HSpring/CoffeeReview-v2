b970f37050546199dd12e8121cae8d6c334c240f
legendmic2 <goonoo0731@gmail.com>
2020-11-29 21:58:10 +0900

    add: AuthVO 생성 및 UserVO, UserMapper(진행중) 수정
    #65

---
+++ src/main/java/com/coffeereview/domain/AuthVO.java
@@ -0,0 +1,23 @@
+package com.coffeereview.domain;
+
+import lombok.Data;
+
+/**
+* @packageName	: com.coffeereview.domain
+* @fileName		: AuthVO.java
+* @author		: Goonoo Jang
+* @date			: 2020.11.29
+* @description	:
+* ===========================================================
+* DATE              AUTHOR             NOTE
+* -----------------------------------------------------------
+* 2020.11.29        Goonoo Jang       최초 생성
+*/
+@Data
+public class AuthVO {
+	
+	private String uid;
+	private String auth;
+	
+
+}
